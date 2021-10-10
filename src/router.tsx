fb4cf7ee013efdb1ebd3c5113c2532f54b8f586a
andoly <andoly.cs@hotmail.com>
2021-10-10 18:10:26 -0300

    SelectOption and FormStep2

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import { FormStep1 } from './steps/FormStep1'
+import { FormStep2 } from './steps/FormStep2';
 
 export const Router = () => {
     return (
       <BrowserRouter>
         <Route path="/" exact component={FormStep1} />
+        <Route path="/step2" exact component={FormStep2} />
       </BrowserRouter>
     );
 }
---
+++ src/steps/FormStep2/index.tsx
@@ -0,0 +1,78 @@
+import { ChangeEvent, useEffect } from "react";
+import { useHistory } from "react-router";
+import { Link } from "react-router-dom";
+import { SelectOption } from "../../component/SelectOption";
+import { Theme } from "../../component/Theme";
+import { useForm, FormActions } from "../../context/FormContext";
+import * as S from "./style";
+
+export const FormStep2 = () => {
+  const { state, dispatch } = useForm();
+  const history = useHistory();
+
+  useEffect(() => {
+    dispatch({
+      type: FormActions.setCurrentStep,
+      payload: 2,
+    });
+  }, []);
+
+  const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: FormActions.setName,
+      payload: event.target.value,
+    });
+  };
+
+  const handleNextStep = () => {
+    if (state.level) {
+      history.push("/step3");
+    } else {
+      alert("Preencha os dados");
+    }
+  };
+
+  const setLevel = (level: number) => {
+    dispatch({
+      type: FormActions.setLevel,
+      payload: level,
+    });
+  }
+
+  return (
+    <Theme>
+      <S.Container>
+        <p>Passo 2/3</p>
+        <h1>{state.name}, o que melhor descreve você?</h1>
+        <p>
+          Escolha a opção que melhor condiz com seu estado atual,
+          profissionalmente.
+        </p>
+
+        <hr />
+
+        <SelectOption
+          title="Sou iniciante"
+          description="Comecei a programar há menos de 2 anos"
+          icon="🥳"
+          selected={state.level === 0}
+          onClick={() => setLevel(0)}
+        />
+
+        <SelectOption
+          title="Sou programador"
+          description="Já programo há 2 anos ou mais"
+          icon="😎"
+          selected={state.level === 1}
+          onClick={() => setLevel(1)}
+        />
+
+        <Link to="/" className="backButton">
+          Voltar
+        </Link>
+
+        <button onClick={handleNextStep}>Próximo</button>
+      </S.Container>
+    </Theme>
+  );
+};
